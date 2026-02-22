ae8cb9fab6cf8edd4423ad9571dda1e3736730e6
yktn3110 <yuki.3110f@gmail.com>
2026-02-22 11:12:08 +0900

    section12

--- s12_case/case.sql
+++ s12_case/case.sql
@@ -15,3 +15,44 @@ GROUP BY
     users.id
 ORDER BY
     user_rank ASC;
+
+/* replace null to 0 */
+select
+    p.id,
+    p.name,
+    CASE
+        WHEN sum(od.product_qty) IS NULL THEN 0
+        ELSE sum(od.product_qty)
+    END AS total_quantity
+
+FROM
+products p
+LEFT OUTER JOIN order_details od
+ON p.id = od.product_id
+GROUP BY
+    p.id
+
+/* practice */
+SELECT
+    p.id,
+    p.name,
+    CASE
+        WHEN sum(od.product_qty) IS NULL THEN 0
+        ELSE sum(od.product_qty)
+    END AS total_quantity
+    ,
+    CASE
+        WHEN sum(od.product_qty) > 20 THEN 'A'
+        WHEN sum(od.product_qty) > 10 THEN 'B'
+        ELSE 'C'
+    END AS rank
+FROM
+    products p
+LEFT OUTER JOIN order_details od
+ON p.id = od.product_id
+GROUP BY
+    p.id
+ORDER BY
+    rank
+
+
