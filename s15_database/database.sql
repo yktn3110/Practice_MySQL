d69cb6ae84c39276301eabdf86104c2803538539
yktn3110 <yuki.3110f@gmail.com>
2026-02-25 08:19:07 +0900

    change table structure

--- s15_database/database.sql
+++ s15_database/database.sql
@@ -12,4 +12,20 @@ create table books(
     id int not null AUTO_INCREMENT PRIMARY KEY,
     title varchar(255) not null);
 show tables;
+show columns from books;
 
+
+/* add columns */
+use book_store;
+show columns from books;
+alter table books add price int after title;
+show columns from books;
+
+/* change column name */
+show columns from books;
+alter table books change price unit_price int;
+show columns from books;
+
+/* delete column */
+alter table books drop column unit_price;
+show columns from books;
